b270c7f88fe3b71d6a98fe90cafd803e878339dc
Sayantan102002 <62782051+Sayantan102002@users.noreply.github.com>
2023-04-13 21:17:53 +0530

    Render url added

--- my-app/src/api/index.js
+++ my-app/src/api/index.js
@@ -1,29 +1,29 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL:'http://localhost:5000'})
-
-API.interceptors.request.use((req)=>{
-    if(localStorage.getItem('Profile')){
+// const API = axios.create({baseURL:'http://localhost:5000'})
+const API = axios.create({ baseURL: "https://customer-counter-backend.onrender.com" })
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('Profile')) {
     req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('Profile')).token}`
-    
-      }
-      return req;
-    })
+
+  }
+  return req;
+})
 
 
 
-export const login =(authdata)=> API.post('/user/login',authdata);
-export const signup =(authdata)=>API.post('/user/signup',authdata);
+export const login = (authdata) => API.post('/user/login', authdata);
+export const signup = (authdata) => API.post('/user/signup', authdata);
 
-export const customer =(customerdata) => API.post('/duedetails/customer',customerdata);
-
-
-export const customerdetails =() => API.get('/duedetails/getdetails');
-export const deleteCustomer=(customerId)=>API.delete(`/duedetails/deleteCustomer/${customerId}`)
-export const editcustomer =(customerId,editcustomerdata)=>API.patch(`/duedetails/editCustomer/${customerId}`,editcustomerdata)
+export const customer = (customerdata) => API.post('/duedetails/customer', customerdata);
 
 
-export const pdetails =(productdata)=>API.post('/addproduct/pdetails',productdata);
-export const fetchproducts =()=>API.get('/addproduct/getpdetails');
-export const deleteproduct =(productId) =>API.delete(`/addproduct/deleteproduct/${productId}`)
-export const updateproduct=(productId,editdetail)=>API.patch(`/addproduct/updateproduct/${productId}`,editdetail)
+export const customerdetails = () => API.get('/duedetails/getdetails');
+export const deleteCustomer = (customerId) => API.delete(`/duedetails/deleteCustomer/${customerId}`)
+export const editcustomer = (customerId, editcustomerdata) => API.patch(`/duedetails/editCustomer/${customerId}`, editcustomerdata)
+
+
+export const pdetails = (productdata) => API.post('/addproduct/pdetails', productdata);
+export const fetchproducts = () => API.get('/addproduct/getpdetails');
+export const deleteproduct = (productId) => API.delete(`/addproduct/deleteproduct/${productId}`)
+export const updateproduct = (productId, editdetail) => API.patch(`/addproduct/updateproduct/${productId}`, editdetail)

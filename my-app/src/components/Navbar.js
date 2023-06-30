3e23e4e2b0f1d546d3ce925f0fad967e93e84192
mr07s <106861707+mr07s@users.noreply.github.com>
2023-06-30 17:50:11 +0530

    responsiveness added

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -77,66 +77,78 @@ const Navbar = () => {
           </div>
         </nav>
   
+
+
+
+
+
+        <div className="navsidebarselect" onClick={openorclosenav}>
+        {
+        nav?(
+          <img src={closecycle} className="closenav"/>
+        ) : (
+          <img src={hamburger} width="10px" className="hamburger" />
+        )}
+      </div>
+
+      {nav ? (
+<div className="navsidebarcontainer">
+     
+        <nav className="navsidebar">
+          <div className="navsidebarleft">
+            {user ? (
+              <ul className="navsidebaritems">
+                <li className="navsidebarlist">
+                  <Link to="/">GalaxyMarble</Link>
+                </li>
+                <li className="navsidebarlist">
+                  <Link to="/duedetails">Duedetails</Link>
+                </li>
+                <li className="navsidebarlist">
+                  <Link to="/addduedetails">AddDuedetails</Link>
+                </li>
+                <li className="navsidebarlist">
+                  <Link to="/addproduct">AddProducts</Link>
+                </li>
+                <li className="navsidebarlist">
+                  <Link to="/showproduct">ShowProducts</Link>
+                </li>
+              </ul>
+            ) : (
+              <></>
+            )}
+          </div>
+
+          <div className="navsidebarright">
+            <ul>
+              {user ? (
+                <button className="sideauthbutton" onClick={Logout}>
+                  <Link to="/login">LogOut</Link>
+                </button>
+              ) : (
+                <Fragment>
+                  <button className="sideauthbutton">
+                    <Link to="/login">LogIn</Link>
+                  </button>
+                  <button className="sideauthbutton">
+                    <Link to="/signup">SignUp</Link>
+                  </button>
+                </Fragment>
+              )}
+            </ul>
+          </div>
+        </nav>
+</div>
+      ) : (
+        <></>
+      )}
+
+  
     </>
   );
   // return (
   //   <>
-  //     <div className="navselect" onClick={openorclosenav}>
-  //       {
-  //       nav?(
-  //         <img src={closecycle} className="closenav"/>
-  //       ) : (
-  //         <img src={hamburger} width="10px" className="hamburger" />
-  //       )}
-  //     </div>
-  //     {nav ? (
-  //       <nav className="navbar">
-  //         <div className="navbarleft">
-  //           {user ? (
-  //             <ul className="navitems">
-  //               <li className="navlist">
-  //                 <Link to="/">GalaxyMarble</Link>
-  //               </li>
-  //               <li className="navlist">
-  //                 <Link to="/duedetails">Duedetails</Link>
-  //               </li>
-  //               <li className="navlist">
-  //                 <Link to="/addduedetails">AddDuedetails</Link>
-  //               </li>
-  //               <li className="navlist">
-  //                 <Link to="/addproduct">AddProducts</Link>
-  //               </li>
-  //               <li className="navlist">
-  //                 <Link to="/showproduct">ShowProducts</Link>
-  //               </li>
-  //             </ul>
-  //           ) : (
-  //             <></>
-  //           )}
-  //         </div>
-
-  //         <div className="navbarright">
-  //           <ul>
-  //             {user ? (
-  //               <button className="navlist authbutton" onClick={Logout}>
-  //                 <Link to="/login">LogOut</Link>
-  //               </button>
-  //             ) : (
-  //               <Fragment>
-  //                 <button className="navlist authbutton">
-  //                   <Link to="/login">LogIn</Link>
-  //                 </button>
-  //                 <button className="navlist authbutton">
-  //                   <Link to="/signup">SignUp</Link>
-  //                 </button>
-  //               </Fragment>
-  //             )}
-  //           </ul>
-  //         </div>
-  //       </nav>
-  //     ) : (
-  //       <></>
-  //     )}
+  
   //   </>
   // );
 };
